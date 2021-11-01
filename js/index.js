70673653515fa1150113f41648981181e5324267
Abhay <abhayrajeshshah@gmail.com>
2021-11-01 18:41:02 +0530

    Changed car to player xD

--- js/index.js
+++ js/index.js
@@ -7,14 +7,19 @@ var scoreText = document.getElementById("score");
 let gameOverText = document.getElementById("scoree");
 let restart = document.getElementById("restart");
 let gameOver = document.querySelector(".gameOver");
-let high = document.getElementById("highscore")
+let high = document.getElementById("highscore");
+let paused = document.getElementById("pause");
 var score=0;
-var blueCar=new carclass();
+var warship=new carclass();
 var sniperUfos = [];
 var shotgunUfos = [];
 var started=false;
 let time=0;
-var ufos=[]
+var ufos=[];
+var pause=false;
+var enemy = document.getElementById("tah_audio");
+var weapon = document.getElementById("weapon");
+let sounds = [enemy,weapon];
 window.onload=function(){
     var framesPerSec=50;
     started=true;
@@ -22,6 +27,8 @@ window.onload=function(){
     ufos[ufos.length-1].reset();
     setInterval(all,1000/framesPerSec);
 }
+
+
 
 restart.addEventListener("mousedown",()=>{
     gameOver.classList.remove("active");
@@ -36,42 +43,44 @@ if(started){
 },1000);
     
 function all(){
-    if(started){
-        moveAll();
-        drawAll();
+    if(!pause){
+        if(started){
+            moveAll();
+            drawAll();
+        }
     }
 }
 
 
 function moveAll(){   
-    ufos.forEach((el,index)=>{el.move(blueCar);el.index=index;blueCar.checkCollision(el);});
-    shotgunUfos.forEach((el,index)=>{el.move(blueCar);el.index=index;blueCar.checkCollision(el);});
-    sniperUfos.forEach((el,index)=>{el.move(blueCar);el.index=index;blueCar.checkCollision(el);});
-    blueCar.carMove();
+    ufos.forEach((el,index)=>{el.move(warship);el.index=index;warship.checkCollision(el);});
+    shotgunUfos.forEach((el,index)=>{el.move(warship);el.index=index;warship.checkCollision(el);});
+    sniperUfos.forEach((el,index)=>{el.move(warship);el.index=index;warship.checkCollision(el);});
+    warship.carMove();
 }
 
-blueCar.reset();
+warship.reset();
 
 function drawAll(){   
     rect(0,0,canvas.width,canvas.height,"black");
     rect(75,40,200,20,"#e22b2b")
     ufos.forEach((el)=>{
-        el.fire(blueCar);
+        el.fire(warship);
         el.draw();
         el.edgeWrap();
     })
     shotgunUfos.forEach((el)=>{
-        el.fire(blueCar);
+        el.fire(warship);
         el.draw();
         el.edgeWrap();
     })
     sniperUfos.forEach((el)=>{
-        el.fire(blueCar);
+        el.fire(warship);
         el.draw();
         el.edgeWrap();
     })
-    blueCar.draw();
-    blueCar.edgeWrap();
+    warship.draw();
+    warship.edgeWrap();
 }
 
 
